05ad75065e2ba21f8fe4d087550e168420fda1de
liliang <649743135@qq.com>
2017-04-14 18:54:42 +0800

    jodd ex

---
+++ src/test/java/com/demo/jodd/lagarto/LagartoParserTest.java
@@ -0,0 +1,17 @@
+package com.demo.jodd.lagarto;
+
+import jodd.lagarto.LagartoParser;
+import jodd.lagarto.TagVisitor;
+import org.junit.Test;
+
+/**
+ * Created by liliang on 2017/4/14.
+ */
+public class LagartoParserTest {
+
+    @Test
+    public void testBase()
+    {
+
+    }
+}
