dac3cf27f10c9d49b6ad37ecc70eb855690bfbae
liliang <649743135@qq.com>
2017-06-29 10:05:07 +0800

    guava CaseFormat

--- src/test/java/com/demo/guava/string/GuavaStringsTest.java
+++ src/test/java/com/demo/guava/string/GuavaStringsTest.java
@@ -3,6 +3,7 @@ package com.demo.guava.string;
 import static com.google.common.base.Strings.*;
 import static org.junit.Assert.*;
 
+import com.google.common.base.CaseFormat;
 import org.junit.Test;
 
 /**
@@ -21,5 +22,14 @@ public class GuavaStringsTest {
     public void padding()
     {
         System.out.println(padStart("1", 2, '0'));
+    }
+
+    @Test
+    public void testCaseFormat()
+    {
+        String data = "test_data";
+        System.out.println(CaseFormat.LOWER_HYPHEN.to(CaseFormat.LOWER_CAMEL, "test-data"));
+        System.out.println(CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, "test_data"));
+        System.out.println(CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, "test_data"));
     }
 }
